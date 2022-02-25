c8ed115a1d07611bc55d44ab36db9749d2dfc8ab
Vinogradov01 <vinogradov.s@nxt.ru>
2022-02-25 10:53:31 +0300

    first task is done

--- DZ/Program.cs
+++ DZ/Program.cs
@@ -1,3 +1,21 @@
 ﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.WriteLine("Давайте поиграем в сравнение чисел");
 
-Console.WriteLine("Hello, World!");
+Console.Write("Введите первое целое число:");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число:");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write("Первое число больше чем второе  ");  Console.Write(" max="); Console.Write(a); Console.Write(" min="); Console.Write(b); 
+}
+else
+{
+    Console.Write("Второе число больше чем первое  ");  Console.Write(" max="); Console.Write(b); Console.Write(" min="); Console.Write(a); 
+}
+*/
