7c2a051734b39cd390b41413cbf6d98d49779997
Vinogradov01 <vinogradov.s@nxt.ru>
2022-03-05 14:21:15 +0300

    twenty-first task is done

--- DZ_Seminar2/Program.cs
+++ DZ_Seminar2/Program.cs
@@ -28,24 +28,27 @@ ________________________________________________________________________________
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-int thirdDigit(int num)
+Console.WriteLine("Давайте узнаем какая цифра стоит на третьем месте в числе которое вы загадали");
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine()); 
+
+string str = Convert.ToString(num);
+
+if (str.Length < 3)Console.WriteLine("Во введённом числе нет третьей цифры.");
+else Console.WriteLine($"Третья цифра числа равна {str[2]}");
+
+//Альтернативный метод решения задачи № 13
+Console.WriteLine("Введите число для получение");
+int n = Convert.ToInt32(Console.ReadLine());
+int d1 = 0, d2 = 0, d3 = 0;
+while (n != 0)
 {
-    int n3 = (num % 10);
-    return n3;
+    d3 = d2;
+    d2 = d1;
+    d1 = n % 10;
+    n /=10;
 }
-Console.WriteLine("Хотите узнать какая цифра находится на третьей позиции в числе?");
-Console.Write("Введите трёхзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int gate = number / 100;
-   
-    if (gate <= 9 && gate >= 1)
-    {
-        Console.Write("Третьей цифрой в числе является = " + thirdDigit(number));
-    }
-    else
-    {
-        Console.Write("Третьей цифры нет!");
-    }
+Console.WriteLine(d3);
 
 ____________________________________________________________________________________________________________________________________
 
@@ -104,17 +107,48 @@ ________________________________________________________________________________
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+*/
+
+
+/*
 _____________________________________________________________________________________________________________________________
 
 // Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Поиск расстояния между точками, если известны коордитаны точек в трёхмерном пространстве");
+double getDistans(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double xDistans = x1 - x2;
+    double yDistans = y1 - y2;
+    double zDistans = z1 - z2;
+    return Math.Sqrt(Math.Pow(xDistans, 2) + Math.Pow(yDistans, 2) + Math.Pow(zDistans, 2));
+}
+double xA, yA, zA, xB, yB, zB;
+double distans;
+Console.Write("Введите координату Х первой точки: ");
+xA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату У первой точки: ");
+yA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z первой точки: ");
+zA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Х второй точки: ");
+xB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату У второй точки: ");
+yB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z dnjhjq точки: ");
+zB = Convert.ToDouble(Console.ReadLine());
+distans = getDistans(xA, yA, zA, xB, yB, zB);
+double disRound = Math.Round(distans);
+Console.WriteLine("Округлённое значение рассоряния = " + disRound);
+
 ____________________________________________________________________________________________________________________________________
 
 // Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
-*/
+
 double cube(double n)
 {
     double nCube = Math.Pow(n, 3);
@@ -130,3 +164,4 @@ while(count <= number)
     Console.Write( ",  " + cube(count));
     count++;
 }
+*/
